754d48f3b68aa3abf9118bef65f07e19449e7a4f
CaioSousavs <joao.sousa@sptech.school>
2025-06-03 11:33:12 -0300

    Alterações no footer

--- API/src/controllers/usuarioController.js
+++ API/src/controllers/usuarioController.js
@@ -1,29 +1,79 @@
 var usuarioModel = require("../models/usuarioModel");
 // var aquarioModel = require("../models/aquarioModel");
-
 function autenticar(req, res) {
-  var email = req.body.email;
-  var senha = req.body.senha;
+    var email = req.body.email;
+    var senha = req.body.senha;
 
-  if (!email || !senha) {
-    res.status(400).send("Email ou senha não fornecidos!");
-  } else {
-    usuarioModel.autenticar(email, senha)
-      .then(resultado => {
-        if (resultado.length === 1) {
-          res.json(resultado[0]);
-        } else if (resultado.length === 0) {
-          res.status(403).send("Email ou senha inválidos");
-        } else {
-          res.status(500).send("Mais de um usuário com as mesmas credenciais");
-        }
-      })
-      .catch(erro => {
-        console.log("Erro ao autenticar usuário:", erro.sqlMessage , erro);
-        res.status(500).json(erro.sqlMessage || erro);
-      });
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else {
+
+        usuarioModel.autenticar(email, senha)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+
+                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        //    .then((resultadoAquarios) => {
+                        //        if (resultadoAquarios.length > 0) {
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha
+                        });
+                        //}
+                        // else {
+                        //      res.status(204).json({ aquarios: [] });
+                        //  }
+                        // })
+                    }
+                    //else
+                    if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
   }
-}
+
+// function autenticar(req, res) {
+//   var email = req.body.email;
+//   var senha = req.body.senha;
+
+//   if (!email || !senha) {
+//     res.status(400).send("Email ou senha não fornecidos!");
+//   } else {
+//     usuarioModel.autenticar(email, senha)
+//       .then(resultado => {
+//         if (resultado.length === 1) {
+//           res.json(resultado[0]);
+//         } else if (resultado.length === 0) {
+//           res.status(403).send("Email ou senha inválidos");
+//         } else {
+//           res.status(500).send("Mais de um usuário com as mesmas credenciais");
+//         }
+//       })
+//       .catch(erro => {
+//         console.log("Erro ao autenticar usuário:", erro.sqlMessage , erro);
+//         res.status(500).json(erro.sqlMessage || erro);
+//       });
+//   }
+// }
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
